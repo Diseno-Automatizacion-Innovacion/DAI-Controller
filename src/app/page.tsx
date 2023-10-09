0a29e8ad64b120f40e9b8f83192e61223150ca16
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-10-09 21:21:20 +0200

    added svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,12 @@ export default function Login() {
 
     const router = useRouter()
 
-    const [loading, setLoading] = useState(<div>hola</div>)
+    const [loading, setLoading] = useState(
+        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></  circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0   12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+    )
 
     useEffect(() => {
         async function checkCookie() {
@@ -23,7 +28,7 @@ export default function Login() {
                 })).json()
                 console.log(res)
                 if (res.ok) router.push("/Dashboard")
-                setLoading(<div></div>)
+                setLoading(<div>Todo lo del login :D</div>)
             }
         }
         checkCookie()
@@ -49,7 +54,7 @@ export default function Login() {
 
     return (
         <main className='flex items-center place-content-center h-[100vh]'>
-            <div className='w-[30vw] h-[50vh] bg-white rounded'>
+            <div className='w-[30vw] h-[50vh] bg-slate-800 rounded text-black'>
                 {loading}
             </div>
         </main>
