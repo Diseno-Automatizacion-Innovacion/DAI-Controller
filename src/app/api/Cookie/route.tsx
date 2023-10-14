3219e0f5fdaa87301cff512f409b28c7869acffc
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-10-14 23:58:10 +0200

    He puesto unos checks para probar interfaz en windows

--- src/app/api/Cookie/route.tsx
+++ src/app/api/Cookie/route.tsx
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as fs from 'fs'
+import * as os from 'os'
 
 
 export async function POST(req: NextRequest) {
     const data = await req.json()
 
-    // console.log("route:", data)
+    if ((process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") && os.platform.name === "win32") {
+        return NextResponse.json({
+            "ok": true,
+        })
+    }
 
     try {
         const cookies = fs.readFileSync("/etc/daiCookies").toString()
--- src/app/api/Login/route.tsx
+++ src/app/api/Login/route.tsx
@@ -1,12 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as fs from 'fs'
 import * as crypto from 'crypto'
+import * as os from 'os'
 
 
 export async function POST(req: NextRequest) {
     const data = await req.json()
 
     console.log(data)
+
+    if ((process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") && os.platform.name === "win32") {
+        return NextResponse.json({
+            "ok": true,
+            "cookie": "test"
+        })
+    }
 
     try {
         const credentials = fs.readFileSync("/etc/daiShadow").toString()
