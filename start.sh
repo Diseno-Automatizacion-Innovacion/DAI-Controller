40e04c499243346ea88dc2ad6036c5140eadf80d
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-10-16 20:41:05 +0200

    Mayusculas mejor puestas

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ "$(git pull 2>&1)" != "Already up to date." ];
   then
-    echo "[Update]: fetching data"
+    echo "[Update]: Fetching data"
     git fetch --all > /dev/null
     echo "[Update]: Writting changes"
     git reset --hard origin/main > /dev/null
     echo "[Update]: Building changes"
     npm run build > /dev/null
-    echo "[Update]: done"
+    echo "[Update]: Done"
   fi
 npm run start
