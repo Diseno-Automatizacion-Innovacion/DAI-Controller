d711c92cee0ae27f709ccd717d33ebfea354ae17
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-10-16 20:42:55 +0200

    Y yo creo que asi se puede quedar

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 if [ "$(git pull 2>&1)" != "Already up to date." ];
   then
-    echo "[Update]: Fetching data"
+    echo "[Update]: Updates available"
+    echo "[Update]: Fetching data..."
     git fetch --all > /dev/null
-    echo "[Update]: Writting changes"
+    echo "[Update]: Writting changes..."
     git reset --hard origin/main > /dev/null
-    echo "[Update]: Building changes"
+    echo "[Update]: Building changes..."
     npm run build > /dev/null
     echo "[Update]: Done"
   fi
+echo "[Update]: Server up to date"
 npm run start
