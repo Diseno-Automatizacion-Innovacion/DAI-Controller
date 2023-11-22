59f9c2df2c7ccd16d1026f2758982779bd8217af
r1d <camamovilcubo@gmail.com>
2023-11-22 17:38:52 +0100

    papa? D:

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { Metadata } from 'next'
+
 import menuIcon from "../../../public/icons/menu.svg"
 
 
 export default function Dashboard() {
+
+    const [toggle, setToggle] = useState(false) // a[0] valor a[1] function
 
     const router = useRouter()
 
@@ -31,10 +33,23 @@ export default function Dashboard() {
     }, [router])
 
     return (
-        <main>
-            <header tabIndex={0} className='fixed flex flex-col justify-center items-center top-0 bg-slate-500 rounded h-full left-0 cursor-pointer focus:hover:w-5/6 hover:w-5/6  duration-200 ease-in-out'>
-                <Image src={menuIcon} alt='Icon' className=''></Image>
+        <main className='bg-slate-900 h-screen'>
+            <header tabIndex={0} className='fixed flex flex-col justify-center items-center top-0 bg-slate-500 rounded h-full left-0 cursor-pointer duration-200 ease-in-out  focus:w-5/6  z-30' onFocus={() => {
+                setToggle(true)
+            }} onBlur={() => {
+                setToggle(false)
+            }}>
+                {
+                    toggle ?
+                        <div>abierto</div>
+                        :
+                        <Image src={menuIcon.src} alt="hola" width={30} height={30} />
+                }
+
             </header>
+            <div id='blur' className={`absolute z-10 w-screen h-screen bg-black ${toggle ? "" : "hidden"} opacity-70 blur-sm`}>
+
+            </div>
         </main>
     )
 }
